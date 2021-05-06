0e9bb23d429f5628a1e0d10f7cc693a1c1d345e4
Jan <jan.kranzler@gmx.de>
2021-05-06 21:06:14 +0200

    bubbel sort and insertion sort done

--- Ex02_CutkovicKranzlerBiermann/Ex02_LastnamesOfMembers_project/src/main/java/de/unistuttgart/vis/dsass2021/ex02/p3/Sorter.java
+++ Ex02_CutkovicKranzlerBiermann/Ex02_LastnamesOfMembers_project/src/main/java/de/unistuttgart/vis/dsass2021/ex02/p3/Sorter.java
@@ -7,16 +7,35 @@ public class Sorter {
 	}
 	
 	public static <T extends Comparable<T>> void insertionSort(ISimpleList<T> list) {
-
-	}
-	
-	public static <T extends Comparable<T>> void bubbleSort(ISimpleList<T> list) {
-		final int n = list.getSize() -2;
-		for (int i=0; i< n) {
-			if (list.get(i) >list.get(i+1)) {
-				list.swap
+		int n = list.size()-1;
+		for (int i; i<n; i++) {
+			T m = list.get(i);
+			int indexOfM=i;
+			leftOfMGreater = true;
+			while (j<0 && leftOfMGreater) {
+				if (firstIsGreaterThanSecond(list.get(j-1),m)) {
+					list.swap(j-1, j);
+					j--;
+				} else {
+					leftOfMGreater = false;
+				}
 			}
 		}
 	}
+	
+	public static <T extends Comparable<T>> void bubbleSort(ISimpleList<T> list) {
+		int n = list.size() -2;
+		while (n>1) {
+			for (int i = 0; i < n; i++) {
+				if (firstIsGreaterThanSecond(list.get(i), list.get(i + 1))) {
+					list.swap(i, i + 1);
+				}
+			}
+			n--;
+		}
+	}
 
+	private satic boolean firstIsGreaterThanSecond(T t1, T t2) {
+		t1.compareTo(t2);
+	}
 }
