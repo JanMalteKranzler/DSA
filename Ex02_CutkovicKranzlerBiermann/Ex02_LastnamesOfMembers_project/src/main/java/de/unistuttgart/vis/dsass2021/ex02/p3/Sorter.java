39dad67b39c3e573fd59c4d60f257e8a0e7b5800
Jan <jan.kranzler@gmx.de>
2021-05-07 21:40:02 +0200

    Hab alles gemacht, die Tests sind aber noch nicht fertig.

--- Ex02_CutkovicKranzlerBiermann/Ex02_LastnamesOfMembers_project/src/main/java/de/unistuttgart/vis/dsass2021/ex02/p3/Sorter.java
+++ Ex02_CutkovicKranzlerBiermann/Ex02_LastnamesOfMembers_project/src/main/java/de/unistuttgart/vis/dsass2021/ex02/p3/Sorter.java
@@ -1,23 +1,41 @@
 package de.unistuttgart.vis.dsass2021.ex02.p3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sorter {
 		
 	public static <T extends Comparable<T>> void selectionSort(ISimpleList<T> list) {
-
+		int p = list.size()-1;
+		while (p>0) {
+			//suche das größte Element aus dem Bereich von 0 bis p
+			T m = list.get(0);
+			int indexOfLargestElement = 0;
+			for (int i=1; i<=p; i++) {
+				if (firstIsGreaterThanSecond(list.get(i), m)) {
+					m = list.get(i);
+				}
+			}
+			//Das größte Element ganz nach rechts legen
+			list.swap(indexOfLargestElement, p);
+			//den Bereich 0-p verkleinern
+			p--;
+		}
 	}
 	
 	public static <T extends Comparable<T>> void insertionSort(ISimpleList<T> list) {
 		int n = list.size()-1;
-		for (int i; i<n; i++) {
-			T m = list.get(i);
+		for (int i =0; i<n; i++) {
+
+			T m = list.get(i);			//wir wählen ein Element m
 			int indexOfM=i;
-			leftOfMGreater = true;
-			while (j<0 && leftOfMGreater) {
-				if (firstIsGreaterThanSecond(list.get(j-1),m)) {
-					list.swap(j-1, j);
-					j--;
+			boolean leftOfMGreater = true;
+			while (indexOfM>0 && leftOfMGreater) {		//solange wir nicht ganz kinks sind und das Element links von m größer m ist
+				if (firstIsGreaterThanSecond(list.get(indexOfM-1),m)) {		//überprüfen ob das element links von m gößer m ist
+					list.swap(indexOfM-1, indexOfM);						//wenn ja vertauschen
+				indexOfM--;													//indexOfM korrigieren
 				} else {
-					leftOfMGreater = false;
+					leftOfMGreater = false;									//wenn nicht sorgen wir dafür, dass die Schleife endet
 				}
 			}
 		}
@@ -35,7 +53,8 @@ public class Sorter {
 		}
 	}
 
-	private satic boolean firstIsGreaterThanSecond(T t1, T t2) {
-		t1.compareTo(t2);
+	//Compares two Java Generics and returns only true, if the first argument is "bigger", than the second argument
+	private static<T extends Comparable<T>> boolean firstIsGreaterThanSecond(T t1, T t2) {
+		return t1.compareTo(t2)<0;
 	}
 }
--- Ex02_CutkovicKranzlerBiermann/Ex02_LastnamesOfMembers_project/src/test/java/de/unistuttgart/vis/dsass2021/ex02/p3/SorterTest.java
+++ Ex02_CutkovicKranzlerBiermann/Ex02_LastnamesOfMembers_project/src/test/java/de/unistuttgart/vis/dsass2021/ex02/p3/SorterTest.java
@@ -9,6 +9,38 @@ import de.unistuttgart.vis.dsass2021.ex02.p3.Sorter;
 
 public class SorterTest {
 
-	// Add your test methods here
+	public void testSelectionSort() {
+		final SimpleList<String> = createRandomList();
 
+	}
+
+	private SimpleList<String> createRandomList() {
+		SimpleList<String> list = new SimpleList();
+		list.append("ghzrj");
+		list.append("agae");
+		list.append("dgs");
+		list.append("");
+		list.append("tui");
+		list.append("ioöouo");
+		list.append("cunt");
+		list.append("ouzu");
+
+		return list;
+	}
+
+	//Checks, whether the list is actually sorted.
+	private boolean listIsSorted(final SimpleList<T extends Comparable<T>> list) {
+		boolean listIsSorted = true;
+		for (int i=0; i< (list.size()-2) ; i++) {
+			if (firstIsGreaterThanSecond(list.get(i), list.get(i+1))) {
+				listIsSorted=false;
+			}
+		}
+		return listIsSorted;
+	}
+
+	//Compares two Java Generics and returns only true, if the first argument is "bigger", than the second argument
+	private <T extends Comparable<T>> boolean firstIsGreaterThanSecond(T t1, T t2) {
+		return t1.compareTo(t2)<0;
+	}
 }
